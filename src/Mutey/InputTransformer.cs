530abdf91e8f67d228c2ecbee9784abb684b4148
Brad Glass <brad.t.glass@gmail.com>
2021-02-03 22:58:24 +0000

    Implement complete logic for transforming hardware input to actions

--- src/Mutey/InputTransformer.cs
+++ src/Mutey/InputTransformer.cs
@@ -4,7 +4,10 @@ using NLog;
 
 namespace Mutey
 {
-    public class MuteController
+    /// <summary>
+    ///     Transforms the input messages into actions to be sent to active calls.
+    /// </summary>
+    public class InputTransformer
     {
         private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
 
@@ -14,7 +17,7 @@ namespace Mutey
 
         private Input? lastInput;
 
-        public MuteController(TimeSpan inputCooldown)
+        public InputTransformer(TimeSpan inputCooldown)
         {
             this.inputCooldown = inputCooldown;
         }
@@ -22,61 +25,69 @@ namespace Mutey
         public bool SmartPtt { get; set; } = true;
 
         public TimeSpan SmartPttActivationDuration { get; set; } = TimeSpan.FromSeconds(1.5);
+        private event EventHandler<MuteAction>? ActionRequired;
 
-        public MuteAction ProcessInput(HardwareType hardwareType, HardwareMessageType messageType)
+        public void Transform(HardwareType hardwareType, HardwareMessageType messageType)
         {
             DateTime now = DateTime.Now;
-            MuteAction action;
 
             if (lastInput is not { } validLastInput)
             {
-                action = ProcessFirstInput(hardwareType, messageType);
+                ProcessFirstInput(hardwareType, messageType);
             }
             else
             {
                 TimeSpan duration = validLastInput.Timestamp - now;
 
                 if (duration < inputCooldown)
-                    return MuteAction.None;
+                    return;
 
                 if (validLastInput.HardwareType == HardwareType.Toggle &&
                     hardwareType == HardwareType.Toggle &&
                     validLastInput.MessageType == HardwareMessageType.StartToggle &&
                     messageType == HardwareMessageType.EndToggle &&
                     duration > SmartPttActivationDuration)
-                    action = MuteAction.Mute;
+                    RaiseAction(MuteAction.Mute);
                 else
-                    action = ProcessFirstInput(hardwareType, messageType);
+                    ProcessFirstInput(hardwareType, messageType);
             }
 
             lastInput = new Input(hardwareType, messageType, now);
-            return action;
         }
 
-        private static MuteAction ProcessFirstInput(HardwareType hardwareType, HardwareMessageType messageType)
+        private void RaiseAction(MuteAction action)
+            => ActionRequired?.Invoke(this, action);
+
+        private void ProcessFirstInput(HardwareType hardwareType, HardwareMessageType messageType)
         {
             if (hardwareType == HardwareType.Unknown)
             {
                 logger.Warn("Cannot process input for unknown hardware");
-                return MuteAction.None;
+                return;
             }
 
             if (messageType == HardwareMessageType.Unknown)
             {
                 logger.Warn("Cannot process input for unknown message type");
-                return MuteAction.None;
+                return;
             }
 
-            return hardwareType switch
+            switch (hardwareType)
             {
-                HardwareType.Toggle => messageType switch
-                {
-                    HardwareMessageType.StartToggle => MuteAction.Toggle,
-                    HardwareMessageType.EndToggle => MuteAction.None,
-                    _ => throw new ArgumentOutOfRangeException(nameof(messageType), messageType, null)
-                },
-                _ => throw new ArgumentOutOfRangeException(nameof(hardwareType), hardwareType, null)
-            };
+                case HardwareType.Toggle:
+                    switch (messageType)
+                    {
+                        case HardwareMessageType.StartToggle:
+                            RaiseAction(MuteAction.Toggle);
+                            return;
+                        case HardwareMessageType.EndToggle:
+                            return;
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(messageType), messageType, null);
+                    }
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(hardwareType), hardwareType, null);
+            }
         }
 
         private readonly struct Input
