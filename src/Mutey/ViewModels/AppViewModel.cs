d47d8d413c18bd26d65c5736c82dab17c2afe628
Brad Glass <brad.t.glass@gmail.com>
2021-03-02 23:59:39 +0000

    Add basic update check in main view model

--- src/Mutey/ViewModels/AppViewModel.cs
+++ src/Mutey/ViewModels/AppViewModel.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using JetBrains.Annotations;
 using Microsoft.Xaml.Behaviors.Core;
 using Mutey.Views;
+using NLog;
 using Prism.Mvvm;
+using Squirrel;
 
 namespace Mutey.ViewModels
 {
     [UsedImplicitly]
     internal class AppViewModel : BindableBase
     {
+        private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
         public AppViewModel(MuteyViewModel mutey, SettingsViewModel settings, Lazy<CompactView> lazyCompactView, Lazy<AboutWindow> lazyAboutView)
         {
             Mutey = mutey;
@@ -18,6 +22,58 @@ namespace Mutey.ViewModels
             QuitCommand = new ActionCommand(() => Application.Current.Shutdown());
             AboutCommand = new ActionCommand(() => lazyAboutView.Value.Show());
             OpenCommand = new ActionCommand(() => lazyCompactView.Value.Show());
+            RunUserInvokedUpdateCheckCommand = new ActionCommand(RunUserInvokedUpdateCheck);
+        }
+
+        private static async void RunUserInvokedUpdateCheck()
+        {
+            try
+            {
+                logger.Info("Beginning update check");
+
+                using IUpdateManager updateManager = await Task.Run(()=> UpdateManager.GitHubUpdateManager(@"https://github.com/G18SSY/Mutey"));
+                UpdateInfo updateInfo = await Task.Run(() => updateManager.CheckForUpdate());
+
+                if (updateInfo.CurrentlyInstalledVersion.Version == updateInfo.FutureReleaseEntry.Version)
+                {
+                    logger.Info("Update check determined latest version is current version");
+                    MessageBox.Show($"Already running latest version ({updateInfo.CurrentlyInstalledVersion.Version})",
+                        "Update", MessageBoxButton.OK, MessageBoxImage.Information);
+                    
+                    return;
+                }
+                
+                logger.Info("Latest version is {Version}", updateInfo.FutureReleaseEntry.Version);
+                MessageBoxResult result = MessageBox.Show(
+                    $"Version {updateInfo.FutureReleaseEntry.Version} is available, would you like to download and install?",
+                    "Update", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                if (result != MessageBoxResult.Yes)
+                {
+                    logger.Info("User chose not to install update");
+                    
+                    return;
+                }
+
+                await updateManager.DownloadReleases(updateInfo.ReleasesToApply);
+                await updateManager.ApplyReleases(updateInfo);
+                await updateManager.CreateUninstallerRegistryEntry();
+
+                logger.Info("Update applied");
+                
+                result = MessageBox.Show("Update applied successfully, would you like to restart?", "Update",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    logger.Info("Restarting application");
+                    UpdateManager.RestartApp();
+                }
+            }
+            catch (Exception e)
+            {
+                logger.Error(e, "Error durign update check");
+            }
         }
 
         public MuteyViewModel Mutey { get; }
@@ -25,8 +81,12 @@ namespace Mutey.ViewModels
         public ICommand QuitCommand { get; }
         
         public ICommand AboutCommand { get; }
+        
         public ICommand OpenCommand { get; }
+        
 
+        public ICommand RunUserInvokedUpdateCheckCommand { get; }
+        
         public SettingsViewModel Settings { get; }
     }
 }
