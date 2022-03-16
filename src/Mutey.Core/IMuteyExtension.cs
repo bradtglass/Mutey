37908c93025b2a60105c142ed28a8eb5b5949119
Brad Glass <brad.t.glass@gmail.com>
2022-03-16 13:27:05 +0000

    Add a base class for extensions to implement

---
+++ src/Mutey.Core/IMuteyExtension.cs
@@ -0,0 +1,44 @@
+namespace Mutey.Core
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Reflection;
+    using System.Windows.Controls;
+
+    public abstract class MuteyExtension : IDisposable
+    {
+        public abstract string Name { get; }
+
+        public abstract string Description { get; }
+
+        public virtual Version Version => Assembly.GetExecutingAssembly().GetName().Version!;
+
+        protected IMutey Mutey { get; }
+
+        protected MuteyExtension( IMutey mutey )
+        {
+            Mutey = mutey;
+        }
+
+        public void Dispose()
+        {
+            Dispose( true );
+            GC.SuppressFinalize( this );
+        }
+
+        public virtual TabItem? LoadSettingsPage()
+        {
+            return null;
+        }
+
+        public abstract void Initialise();
+
+        [ SuppressMessage( "ReSharper", "UnusedParameter.Global" ) ]
+        protected virtual void Dispose( bool disposing ) { }
+
+        ~MuteyExtension()
+        {
+            Dispose( false );
+        }
+    }
+}
